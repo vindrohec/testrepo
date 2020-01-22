638da9747b4bdf3936b88dd6e977ea71adcb5939
Karol Zedlewski <vindrohec@gmail.com>
2020-01-22 15:23:25 +0100

    dodałem plik .gitignore

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main() {
     printf("Ala ma kota\n");
-    printf("Chyba już wiem jak pushować w githubie");
-    printf("Nie wiem jak w readme przejść do następnej linii xD");
+    printf("Chyba już wiem jak pushować w githubie\n");
+    printf("Nie wiem jak w readme przejść do następnej linii xD\n");
+    printf("Dodałem plik .gitignore dla plików *.o i *.out\n");
 return 0;
 }
