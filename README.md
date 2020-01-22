838e133b231a05315eb1253b54bbbd2d88f70e59
Karol Zedlewski <vindrohec@gmail.com>
2020-01-22 15:10:48 +0100

    najnowsza wersja

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # testrepo
-tylko do nauki
+tylko do nauki\n 
 uczymy się pushować
+\n
+zmiany w pliku main.c
--- main.c
+++ main.c
@@ -2,5 +2,6 @@
 
 int main() {
     printf("Ala ma kota\n");
+    printf("Chyba już wiem jak pushować w githubie");
 return 0;
 }
